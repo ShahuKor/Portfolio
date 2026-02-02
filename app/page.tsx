ea7014cc2f19e8aaba593c89408893ae0b3da5d1
ShahuKor <korshahu17@gmail.com>
2026-02-02 16:18:05 +0000

    Complete NavBar with motion

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Hero } from "@/components/hero";
 export default function Home() {
   return (
     <>
-      <Container className="min-h-screen p-4 md:p-10">
+      <Container className="min-h-[200vh] p-4 md:pt-20 md:pb-10">
         <Hero />
       </Container>
     </>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,9 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
 import { Container } from "./Container";
 import Image from "next/image";
-import { motion } from "framer-motion";
+import { motion, useMotionValueEvent, useScroll } from "framer-motion";
 
 export const Navbar = () => {
   const navItems = [
@@ -12,9 +12,33 @@ export const Navbar = () => {
     { title: "Contact", href: "/contact" },
     { title: "Blog", href: "/blog" },
   ];
+
+  const [hovered, setHovered] = useState<number | null>(null);
+  const { scrollY } = useScroll();
+  const [scrolled, setScrolled] = useState<boolean>(false);
+
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest > 20) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  });
+
   return (
     <Container>
-      <div className="flex items-center justify-between p-2">
+      <motion.nav
+        animate={{
+          boxShadow: scrolled ? "var(--shadow-custom)" : "none",
+          width: scrolled ? "55%" : "100%",
+          y: scrolled ? 10 : 0,
+        }}
+        transition={{
+          duration: 0.4,
+          ease: "easeInOut",
+        }}
+        className="fixed inset-x-0 top-0 z-50 mx-auto flex max-w-4xl items-center justify-between rounded-full bg-white px-3 py-2 dark:bg-neutral-900"
+      >
         <Image
           className="h-10 w-10 rounded-full"
           src="/profile.jpeg"
@@ -26,12 +50,24 @@ export const Navbar = () => {
         />
         <div className="flex items-center justify-center">
           {navItems.map((item, index) => (
-            <Link href={item.href} key={index} className="text-sm">
-              {item.title}
+            <Link
+              href={item.href}
+              key={index}
+              className="relative px-2 py-1 text-sm"
+              onMouseEnter={() => setHovered(index)}
+              onMouseLeave={() => setHovered(null)}
+            >
+              {hovered === index && (
+                <motion.span
+                  layoutId="hovered-span"
+                  className="absolute inset-0 h-full w-full rounded-md bg-neutral-100 dark:bg-neutral-800"
+                ></motion.span>
+              )}
+              <span className="relative z-10">{item.title}</span>
             </Link>
           ))}
         </div>
-      </div>
+      </motion.nav>
     </Container>
   );
 };
