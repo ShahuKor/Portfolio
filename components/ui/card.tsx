fc076ca32f9dd34eb95c9f484faa49a2f7f20ab9
ShahuKor <korshahu17@gmail.com>
2026-02-03 23:13:35 +0000

    fix positioning issue in cards

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { CiGlobe } from "react-icons/ci";
 import { FaGithub } from "react-icons/fa";
+
 interface CardProps {
   src: string;
   title: string;
@@ -32,32 +33,34 @@ export default function Card({
         />
       </div>
 
-      {/* Content Section */}
-      <div className="flex flex-col gap-3 p-4">
+      <div className="flex flex-1 flex-col gap-3 p-4">
         {/* Title */}
         <h3 className="text-xl font-medium tracking-tight text-(--color-primary)">
           {title}
         </h3>
 
-        {/* Description */}
-        <p className="pb-2 text-base font-medium text-(--color-secondary)">
+        <p className="flex-1 pb-2 text-base font-medium text-(--color-secondary)">
           {description}
         </p>
-        <p className="pb-1 text-base font-medium text-(--color-secondary)">
-          Links
-        </p>
-        <div className="flex gap-2">
-          <Link href={githublink}>
-            <FaGithub className="size-6" />
-          </Link>
-          {livelink && (
-            <Link href={livelink}>
-              <CiGlobe className="size-6" />
+
+        <div className="pb-8">
+          <p className="pb-3 text-base font-medium text-(--color-secondary)">
+            Links
+          </p>
+          <div className="flex gap-2">
+            <Link href={githublink}>
+              <FaGithub className="size-6" />
             </Link>
-          )}
+            {livelink && (
+              <Link href={livelink}>
+                <CiGlobe className="size-6" />
+              </Link>
+            )}
+          </div>
         </div>
       </div>
-      <div className="flex items-center justify-between px-4 pt-6">
+
+      <div className="flex items-center justify-between px-4 pb-10">
         <Link
           href={projectlink}
           className="text-sm font-medium text-neutral-800 underline-offset-4 hover:underline dark:text-neutral-200"
